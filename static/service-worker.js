14774011da3d2548753e7ea013e7bd131a57779e
carsonmckinstry <carson.mckinstry2021@gmail.com>
2024-11-13 19:37:27 -0600

    Static version

---
+++ static/service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'clustering-app-v1';
+const urlsToCache = [
+    '/',
+    '/static/css/styles.css', // Your CSS files
+    '/static/js/script.js',    // Any JavaScript files
+    '/static/images/logo.png', // Any images you want to cache
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(urlsToCache);
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
