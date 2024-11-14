ff2d345a958e701ed437bd1e3cc214f46c0bc465
carsonmckinstry <carson.mckinstry2021@gmail.com>
2024-11-13 20:01:35 -0600

    Static version

--- static/service-worker.js
+++ static/service-worker.js
@@ -5,29 +5,41 @@ const urlsToCache = [
 ];
 
 self.addEventListener('install', event => {
+    console.log('Service worker installing...');
     event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => {
-            return cache.addAll(urlsToCache);
-        })
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                console.log('Caching resources...');
+                return cache.addAll(urlsToCache);
+            })
     );
 });
 
 self.addEventListener('fetch', event => {
+    console.log('Fetch request for:', event.request.url);
     event.respondWith(
-        caches.match(event.request).then(response => {
-            // Return the cached response if found, otherwise fetch from the network
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    console.log('Serving from cache:', response);
+                    return response;
+                }
+                console.log('Fetching from network:', event.request.url);
+                return fetch(event.request);
+            })
+            .catch(() => caches.match('/')) // Fall back to root if network fails
     );
 });
 
 self.addEventListener('activate', event => {
+    console.log('Service worker activating...');
     const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then(cacheNames => {
             return Promise.all(
                 cacheNames.map(cacheName => {
                     if (!cacheWhitelist.includes(cacheName)) {
+                        console.log('Deleting old cache:', cacheName);
                         return caches.delete(cacheName);
                     }
                 })
